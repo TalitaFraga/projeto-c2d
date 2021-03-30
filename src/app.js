5ac147682cfe5a5de7a121bee500be3790f21a9c
TalitaFraga <talitad.fraga@gmail.com>
2021-03-30 15:09:13 -0300

    servidor rodando

--- src/app.js
+++ src/app.js
@@ -4,4 +4,18 @@ const app = express()
 app.use(express.json())
 
 const index = require("./routes/index")
-const 
+const usuarios = require("./routes/usuarioRoute")
+
+app.use(function (req, res, next) {
+    res.header("Acess-Control-Allow-Origin", "*")
+    res.header(
+        "Acess-Control-Allow-Headers",
+        "Origin, X-Request-with, Content-Type, Accept"
+    )
+    next()
+})
+
+app.use("/", index)
+app.use("/usuarios", usuarios)
+
+module.exports = app
